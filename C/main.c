d71927786b50e893d4cf57a260a57c6ad127492e
yuesir <yueqingyou@gmail.com>
2021-03-14 22:19:10 +0800

    Initial Commit

---
+++ C/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  C
+//
+//  Created by yuesir on 2021/3/14.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
